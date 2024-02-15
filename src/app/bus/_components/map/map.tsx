cacdba230b637c15f18bb2df9443d5da7782647a
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-02-15 11:44:44 +0800

    feature: map flyto

--- src/app/bus/_components/map/map.tsx
+++ src/app/bus/_components/map/map.tsx
@@ -1,7 +1,7 @@
 "use client"
-import { MapContainer, Marker, TileLayer, Polyline, Tooltip } from 'react-leaflet'
+import { MapContainer, Marker, TileLayer, Polyline, Tooltip, useMap } from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
-import { useMemo } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { api } from '~/trpc/react'
 import * as BusAtom from "~/state/bus"
 import { useAtom } from "jotai"
@@ -11,7 +11,7 @@ export default function Map() {
     const position = useMemo(()=>({ lat: 24.137396608878987, lng: 120.68692065044608 }), [])
 
     return (
-        <MapContainer center={[position.lat, position.lng]} zoom={13} scrollWheelZoom={true} className="w-full h-full z-0" zoomControl={false} >
+        <MapContainer center={position} zoom={13} scrollWheelZoom={true} className="w-full h-full z-0" zoomControl={false} >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -23,12 +23,31 @@ export default function Map() {
 }
 
 const ShowPolyLines = () => {
+
     const [bus] = useAtom(BusAtom.busAtom)
     const [direction] = useAtom(BusAtom.directionAtom)
     const [headSign] = useAtom(BusAtom.headSignAtom)
     const data = api.bus.getBusGeometry.useQuery(bus, {
         enabled: Boolean(bus),
     })
+    const map = useMap()
+
+    useEffect(()=>{
+        if (data.isSuccess) {
+            const positionStr = data.data[Number(direction)]?.Geometry
+            const regex = /[A-Z()]/g
+            const positionArr = positionStr?.replace(regex, "").split(",").map(f=>f.split(" ").reverse().map(item=>Number(item))) as [number, number][]
+            if (positionArr) {
+                const center = {lat:0, lng:0} as {lat:number, lng:number}
+                positionArr.forEach((d, _i, arr)=>{
+                    center.lat += d[0]/arr.length
+                    center.lng += d[1]/arr.length
+                })
+                map.flyTo(center, 13)
+            }
+        }
+    },[data.data])
+
     if (!data.data || bus === "" || direction === "") {
         return ""
     }
@@ -36,7 +55,8 @@ const ShowPolyLines = () => {
     if (positionStr) {
         const regex = /[A-Z()]/g
         const positionArr = positionStr.replace(regex, "").split(",").map(f=>f.split(" ").reverse().map(item=>Number(item))) as [number, number][]
-        return <Polyline positions={positionArr} pathOptions={{
+        return (
+        <Polyline positions={positionArr} pathOptions={{
             color: "#809fff",
             weight: 8
         }} >
@@ -44,6 +64,7 @@ const ShowPolyLines = () => {
                 {headSign ? headSign : bus}
             </Tooltip>
         </Polyline>
+        )
     }
     return ""
 }
