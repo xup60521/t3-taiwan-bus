714a8f3bcb1618bf56aa2810d97118fe280e5590
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-02-16 23:41:16 +0800

    home page decorate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,42 @@
 'use client'
+import { ScrollArea } from "@radix-ui/react-scroll-area";
 import Link from "next/link";
 import { useState } from "react";
+import { FiMenu } from "react-icons/fi";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "~/components/ui/dropdown-menu";
 
 export default function Home() {
 
-  const [value, setValue] = useState("Taichung")
 
   return (
-    <main className="flex min-h-screen flex-col gap-4 items-center justify-center bg-slate-800 text-white">
+    <main className="flex h-screen flex-col gap-4 items-center justify-center bg-slate-800 text-white">
       <h1 className="font-mono text-3xl">T3 Taiwan Bus</h1>
-      <select className="text-black rounded p-2" value={value} onChange={(e)=>setValue(e.target.value)} defaultValue={"Taichung"}>
-        <option value="Taipei">Taipei</option>
-        <option value="NewTaipei">NewTaipei</option>
-        <option value="Taoyuan">Taoyuan</option>
-        <option value="Taichung">Taichung</option>
-        <option value="Tainan">Tainan</option>
-        <option value="Kaohsiung">Kaohsiung</option>
-        <option value="Keelung">Keelung</option>
-        <option value="KinmenCounty">KinmenCounty</option>
-        <option value="Hsinchu">Hsinchu</option>
-        <option value="HsinchuCounty">HsinchuCounty</option>
-        <option value="MiaoliCounty">MiaoliCounty</option>
-        <option value="ChanghuaCounty">ChanghuaCounty</option>
-        <option value="NantouCounty">NantouCounty</option>
-        <option value="YunlinCounty">YunlinCounty</option>
-        <option value="ChiayiCounty">ChiayiCounty</option>
-        <option value="Chiayi">Chiayi</option>
-        <option value="PingtungCounty">PingtungCounty</option>
-        <option value="YilanCounty">YilanCounty</option>
-        <option value="HualienCounty">HualienCounty</option>
-        <option value="TaitungCounty">TaitungCounty</option>
-        <option value="PenghuCounty">PenghuCounty</option>
-      </select>
-      <Link href={`/bus?city=${value ?? "Taichung"}`} className="font-mono">
-        ---Click Here---
-      </Link>
+      
+        <div className="w-full grid grid-cols-2 h-[70vh] items-center">
+          <div className="text-center"><Link href="/bus?city=Taipei">Taipei</Link></div>
+          <div className="text-center"><Link href="/bus?city=NewTaipei">NewTaipei</Link></div>
+          <div className="text-center"><Link href="/bus?city=Taoyuan">Taoyuan</Link></div>
+          <div className="text-center"><Link href="/bus?city=Taichung">Taichung</Link></div>
+          <div className="text-center"><Link href="/bus?city=Tainan">Tainan</Link></div>
+          <div className="text-center"><Link href="/bus?city=Kaohsiung">Kaohsiung</Link></div>
+          <div className="text-center"><Link href="/bus?city=Keelung">Keelung</Link></div>
+          <div className="text-center"><Link href="/bus?city=KinmenCounty">KinmenCounty</Link></div>
+          <div className="text-center"><Link href="/bus?city=Hsinchu">Hsinchu</Link></div>
+          <div className="text-center"><Link href="/bus?city=HsinchuCounty">HsinchuCounty</Link></div>
+          <div className="text-center"><Link href="/bus?city=MiaoliCounty">MiaoliCounty</Link></div>
+          <div className="text-center"><Link href="/bus?city=ChanghuaCounty">ChanghuaCounty</Link></div>
+          <div className="text-center"><Link href="/bus?city=NantouCounty">NantouCounty</Link></div>
+          <div className="text-center"><Link href="/bus?city=YunlinCounty">YunlinCounty</Link></div>
+          <div className="text-center"><Link href="/bus?city=ChiayiCounty">ChiayiCounty</Link></div>
+          <div className="text-center"><Link href="/bus?city=Chiayi">Chiayi</Link></div>
+          <div className="text-center"><Link href="/bus?city=PingtungCounty">PingtungCounty</Link></div>
+          <div className="text-center"><Link href="/bus?city=YilanCounty">YilanCounty</Link></div>
+          <div className="text-center"><Link href="/bus?city=HualienCounty">HualienCounty</Link></div>
+          <div className="text-center"><Link href="/bus?city=TaitungCounty">TaitungCounty</Link></div>
+          <div className="text-center"><Link href="/bus?city=PenghuCounty">PenghuCounty</Link></div>
+
+        </div>
+      
     </main>
   );
 }
