5cfa2f900ea646eae62c6d6f39541b07f058df48
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-02-16 23:54:27 +0800

    remove ssr

---
+++ src/app/api/getAll/route.ts
@@ -0,0 +1,17 @@
+import {NextResponse, type NextRequest } from "next/server";
+import { get_access_token } from "~/server/api/routers/bus";
+import {type BusList } from "~/type/bus";
+
+export async function GET(req:NextRequest) {
+    const city = req.nextUrl.searchParams.get("city") ?? "Taichung"
+    const access_token_res = (await get_access_token())
+    const access_token = access_token_res.access_token
+    const initBusList_res = await fetch(`https://tdx.transportdata.tw/api/basic/v2/Bus/Route/City/${city}?&$select=SubRoutes,RouteName`, {
+        headers: {
+            "Authorization": `Bearer ${access_token}`
+        },
+        next: {revalidate: 43200}
+    }).then(res => res) 
+    const initBusList = await initBusList_res.json() as BusList[]
+    return NextResponse.json(initBusList)
+}
--- src/app/bus/bus.tsx
+++ src/app/bus/bus.tsx
@@ -4,23 +4,32 @@ import type { BusList } from "~/type/bus"
 import DrawerSection from "~/app/bus/_components/drawer/drawer"
 import SyncSearchParams from "~/lib/SyncSearchParams"
 import BusCard from "~/app/bus/_components/card/busCard"
-import { Suspense } from "react"
+import { Suspense, useEffect, useState } from "react"
 import PageController from "~/app/bus/_components/pageController/pageController"
 import * as BusAtom from "~/state/bus"
 import { useAtom } from "jotai"
 import StationCard from "~/app/bus/_components/card/stationCard"
 import PopupSection from "~/app/bus/_components/popup/popup"
+import { useSearchParams } from "next/navigation"
 
 const Map = dynamic(()=>import("~/app/bus/_components/map/map"), {ssr: false})
 
-export default function Bus({
-    initBusList,
-}: {
-    initBusList: BusList[],
-}) {
+export default function Bus() {
 
     const [page] = useAtom(BusAtom.pageAtom)
     const [openPopup] = useAtom(BusAtom.openPopupAtom)
+    const [initBusList, setinitBusList] = useState<BusList[] | null>(null)
+    const city = useSearchParams().get("city")
+
+    useEffect(()=>{
+        fetch(`/api/getAll?city=${city}`).then(async (res)=>await res.json() as BusList[]).then(data=>{
+            setinitBusList([...data])
+        }).catch(err=>alert(err))
+    },[])
+
+    if (!initBusList) {
+        return "loading..."
+    }
     
     return (
         <>
