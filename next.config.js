f55fd647bb6a4ef3d0d8eecd17709773fda5aa3c
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-02-21 19:21:46 +0800

    add pwa

--- next.config.js
+++ next.config.js
@@ -2,9 +2,13 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+import withPWAInit from "@ducanh2912/next-pwa";
 await import("./src/env.js");
 
+const withPWA = withPWAInit({
+  dest: "public",
+});
 /** @type {import("next").NextConfig} */
 const config = {};
 
-export default config;
+export default withPWA(config);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
-
+import type { Metadata, Viewport } from "next";
 import { TRPCReactProvider } from "~/trpc/react";
 
 const inter = Inter({
@@ -9,10 +9,50 @@ const inter = Inter({
   variable: "--font-sans",
 });
 
-export const metadata = {
-  title: "T3 Taiwan Bus",
-  description: "Taiwan Bus Tracker built with create-t3-app",
-  icons: [{ rel: "icon", url: "/icon.png" }],
+
+const APP_NAME = "PWA App";
+const APP_DEFAULT_TITLE = "My Awesome PWA App";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "Best PWA app in the world!";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
